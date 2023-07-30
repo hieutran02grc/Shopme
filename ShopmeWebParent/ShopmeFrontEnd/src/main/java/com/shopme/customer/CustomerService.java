b11cb4e4e3f7124d1616f53fc36ddbe103c714db
Hieuxuog <hieutran02grc@gmail.com>
2023-07-30 17:33:25 +0700

    login with facebook

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerService.java
@@ -69,14 +69,15 @@ public class CustomerService {
 		}
 	}
 	
-	public void addNewCustomerUponOAuthLogin(String name, String email, String countryCode) {
+	public void addNewCustomerUponOAuthLogin(String name, String email, String countryCode,
+			AuthenticationType authenticationType) {
 		Customer customer = new Customer();
 		customer.setEmail(email);
 		setName(name, customer);
 		
 		customer.setEnabled(true);
 		customer.setCreatedTime(new Date());
-		customer.setAuthenticationType(AuthenticationType.GOOGLE);
+		customer.setAuthenticationType(authenticationType);
 		customer.setPassword("");
 		customer.setAddressLine1("");
 		customer.setCity("");
@@ -97,7 +98,7 @@ public class CustomerService {
 			String firstName = nameArray[0];
 			customer.setFirstName(firstName);
 			
-			String lastName = name.replaceFirst(firstName, "");
+			String lastName = name.replaceFirst(firstName + " ", "");
 			customer.setLastName(lastName);
 		}
 	}
